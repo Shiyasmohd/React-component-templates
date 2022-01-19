0f879e5e8bec362da728cc2ce9e3e8f71bafb89a
Shiyasmohd <shiyasjaseena124@gmail.com>
2022-01-19 17:40:09 +0530

    Contact Us Component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import About from './Components/About Us/About';
 import Carousel from './Components/Carousel/ImageSlider'
+import Contact from './Components/Contact/Contact';
 
 function App() {
   return (
     <div>
        {/* <About/> */}
        {/* <Carousel/> */}
+       {/* <Contact/> */}
     </div>
   );
 }
---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Contact.css';
+
+function Question() {
+    return (
+    <div class="q-container">
+        <div class="q-main">
+            <div class="q-content">
+                <h3>Have Any Questions? </h3>
+                <p>We are here to help. Please fell free to contact us.</p>
+            </div> 
+            <div class="q-btn">
+                <button>Contact Us</button>
+            </div>
+        </div>
+    </div>
+        
+    )
+}
+
+export default Question;
